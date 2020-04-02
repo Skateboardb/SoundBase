21f02762fbcf2fbeaa0480713dbdc2cbe3b9769f
Branford Shaw <shaw.branford@gmail.com>
2020-04-02 16:29:07 -0500

    added nodemon and dev/prod start scripts

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../index.js');
+const app = require('../server.js');
 
 describe('GET/', function() {
 	it('responde with hello world', function(done) {
