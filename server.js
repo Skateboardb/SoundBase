4120aedd4c45905ffb882c94878332134a91ab17
Branford Shaw <shaw.branford@gmail.com>
2020-04-13 15:30:52 -0500

    added extra port

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get('/', function(req, res) {
 //listen to port 3000 by default
 console.log(process.env.PORT);
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000 || 3001);
 
 module.exports = app;
